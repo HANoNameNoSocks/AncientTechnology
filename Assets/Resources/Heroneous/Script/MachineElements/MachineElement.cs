c96a0f871aeee3cca7d088348b26c22f44e42a31
Tavioxsensei <francois.tavin@serendip-it.fr>
2016-08-28 16:49:49 +0200

    Prepare Machine Elements

---
+++ Assets/Resources/Heroneous/Script/MachineElements/MachineElement.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public enum ElementState { NORMAL, THROWN };
+
+public class MachineElement : MonoBehaviour {
+  private Greek owner;
+  private Greek thrower;
+  private Rigidbody2D elementBody;
+  private ElementState state = ElementState.NORMAL;
+
+	// Use this for initialization
+	void Start () {
+    elementBody = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    print(elementBody.velocity.magnitude);
+    if (elementBody.velocity.magnitude <= 2) {
+      state = ElementState.NORMAL;
+    }
+    if (elementBody.velocity.magnitude <= 0) {
+      thrower = null;
+    }
+	}
+
+  public void setOwner(Greek owner) {
+    this.owner = owner;
+    thrower = null;
+    elementBody.isKinematic = true;
+    transform.parent = owner.transform;
+    transform.position = owner.getHeadPosition();
+  }
+
+  public void thrown(Vector2 force) {
+    state = ElementState.THROWN;
+    thrower = owner;
+    owner = null;
+    transform.parent = null;
+    elementBody.isKinematic = false;
+    elementBody.AddForce(force);
+  }
+
+  public ElementState getState() {
+    return state;
+  }
+
+  public Greek getThrower() {
+    return thrower;
+  }
+}
--- Assets/Resources/Heroneous/Script/Players/Greek.cs
+++ Assets/Resources/Heroneous/Script/Players/Greek.cs
@@ -1,22 +1,26 @@
 ﻿using UnityEngine;
 
-public enum State { NORMAL, DASHING, SHOCKED };
+public enum GreekState { NORMAL, DASHING, SHOCKED };
 
 public class Greek : MonoBehaviour {
 
   public int Player;
-  private int Speed = 10000;
+  private int Speed = 5000;
+  private Vector3 decalageTete = new Vector3(0, 0.5f, 0);
   private Controller controller;
 
   private Rigidbody2D characterBody;
 
-  private State state = State.NORMAL;
+  private GreekState state = GreekState.NORMAL;
   
   private double coolDownState = 0;
   private double coolDownDash = 0;
 
   private Vector3 nextForce = Vector3.zero;
-  
+
+  MachineElement possession;
+
+
   void Start () {
     controller = ControllerManager.Instance.Controllers[Player];
     characterBody = GetComponent<Rigidbody2D>();
@@ -25,16 +29,16 @@ public class Greek : MonoBehaviour {
   #region states and draw
 
   void Update() {
-    updateCoolDowns();
+    updateCoolDowns(Time.deltaTime);
 
-    if ((state == State.DASHING || state == State.SHOCKED) && coolDownState <= 0) {
-      state = State.NORMAL;
+    if ((state == GreekState.DASHING || state == GreekState.SHOCKED) && coolDownState <= 0) {
+      state = GreekState.NORMAL;
     }
   }
 
-  void updateCoolDowns() {
-    coolDownState -= Time.deltaTime;
-    coolDownDash -= Time.deltaTime;
+  void updateCoolDowns(float elapsedTime) {
+    coolDownState -= elapsedTime;
+    coolDownDash -= elapsedTime;
   }
 
   #endregion
@@ -43,46 +47,74 @@ public class Greek : MonoBehaviour {
 
   void FixedUpdate() {
 
-    if (state != State.DASHING) {
+    if (state != GreekState.DASHING) {
       characterBody.velocity = Vector3.zero;
     }
     characterBody.AddForce(nextForce);
     nextForce = Vector3.zero;
     
     // normal : can move
-    if (state == State.NORMAL) {
-      nextForce = new Vector2(Speed * Time.deltaTime * controller.GetInGameHorizontal(), -Speed * Time.deltaTime * controller.GetInGameVertical());
-      if (controller.GetInGameDash() && canDash()) {
+    if (state == GreekState.NORMAL) {
+      nextForce = new Vector2(Speed * Time.fixedDeltaTime * controller.GetInGameHorizontal(), -Speed * Time.fixedDeltaTime * controller.GetInGameVertical());
+      if (tired()) {
+        nextForce *= 0.5f;
+      }
+      if (controller.GetInGameDash()) {        
         dash();
       }
     }
   }
 
   void OnCollisionEnter2D(Collision2D collision) {
+    // Player
     Greek zorba = collision.gameObject.GetComponent<Greek>();
 
-    if (zorba != null && state == State.DASHING) {
-      zorba.shock(collision.relativeVelocity);
+    if (zorba != null && state == GreekState.DASHING) {
+      zorba.shock();
       nextForce = Vector3.zero;
+    }
+
+    // Element
+    MachineElement element = collision.gameObject.GetComponent<MachineElement>();
+    if (element != null) {
+      // thrown element ?
+      if (element.getState() == ElementState.THROWN) {
+        shock();
+      } else {
+        element.setOwner(this);
+        possession = element;
+      }
     }
   }
 
-  void shock(Vector2 velocity) {
-    //print(velocity);
-    //nextForce = -velocity;
-    state = State.SHOCKED;
+  void shock() {
+    state = GreekState.SHOCKED;
     coolDownState = 3;
   }
 
   void dash() {
-    nextForce *= 3;
-    state = State.DASHING;
-    coolDownState = 0.2f;
-    coolDownDash = 1;
+    if (possession != null) {
+      throwElement();
+      coolDownDash = 1;
+    } else if (!tired()) {
+      nextForce *= 3;
+      state = GreekState.DASHING;
+      coolDownState = 0.2f;
+      coolDownDash = 1;
+    }
   }
 
-  bool canDash() {
-    return coolDownDash <= 0;
+  void throwElement() {
+    possession.thrown(nextForce*3);
+    possession = null;
+  }
+
+  bool tired() {
+    return coolDownDash > 0;
+  }
+
+  public Vector3 getHeadPosition() {
+    return transform.position + decalageTete;
   }
 
   #endregion
