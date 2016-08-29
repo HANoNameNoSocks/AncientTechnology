dd0dad4b6f6a07c02fd0accb757d950e4641ef0a
BaptisteGaitet <baptiste.gaitet@gmail.com>
2016-08-29 16:28:40 +0200

    add Main Game rules

---
+++ Assets/Resources/Heroneous/Script/GameRules/GameHandler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameHandler : MonoBehaviour {
+
+  public int ScoreToWin;
+  public GameObject[] players;
+
+	// Use this for initialization
+	void Start () {
+
+    for (int i = 0; i < 4; i++) {
+      if (GameManager.Instance.isPlayerIn (i)) {
+        players[i].gameObject.SetActive (true);
+      } else {
+        players[i].gameObject.SetActive (false);
+      }
+    }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    for (int i = 0; i < 4; i++) {
+      if (players [i].GetComponent<Greek> ().getScore () >= ScoreToWin) {
+        print ("player " + (i + 1) + " won !");
+      }
+    }
+	}
+}
